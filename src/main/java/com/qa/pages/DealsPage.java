6c726fe7ec76688ff1b99f8acd90ad8845a2c1c6
AutoMasters34 <cakhilareddy.26@gmail.com>
2020-09-21 13:08:02 +0530

    added intialization part

--- src/main/java/com/qa/pages/DealsPage.java
+++ src/main/java/com/qa/pages/DealsPage.java
@@ -1,8 +1,11 @@
 package main.java.com.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class DealsPage extends LoginPage {
 	public void DealsPage()
 	{
+		WebDriver driver;
 		System.out.println("DealsPage");
 	}
 
